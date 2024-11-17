5292420b57d5a54516f2eb99145b2676b20be391
Priti Burfield-Mills <pri@codi.nz>
2024-11-18 11:21:02 +1300

    fix: minor tweaks on the layout app

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
               />
               <div className="min-h-screen w-full backdrop-blur-3xl dark:bg-black/80 absolute inset-0 z-0">
                 <Image
-                  className="w-full h-full absolute inset-0 object-cover object-center -translate-y-1/4 translate-x-[10%] scale-[2] dark:scale-[2.5] max-md:hidden transition-transform"
+                  className="w-full h-full absolute inset-0 object-cover object-center -translate-y-1/4 translate-x-[10%] scale-[2] max-md:hidden transition-transform"
                   src="/decoration.svg"
                   alt="Next.js logo"
                   fill
@@ -58,9 +58,13 @@ export default function RootLayout({
               </div>
             </div>
             {/* Header */}
-            <header className="w-full flex justify-between items-center">
-              <h1>CAP UK</h1>
-              <UserButton showName />
+            <header className="w-full flex justify-between items-center relative z-0 py-3 px-8">
+              <h1 className="text-4xl font-semibold px-4 py-2 rounded-full backdrop-blur-sm bg-white/20">
+                Web Launch 
+              </h1>
+              <div className="p-2 pt-1 bg-white/20 backdrop-blur-sm text-xl rounded-full">
+                <UserButton showName />
+              </div>
             </header>
             {/* Content */}
             <SignedOut>
@@ -69,7 +73,9 @@ export default function RootLayout({
               </div>
             </SignedOut>
             <SignedIn>
-              <div className="min-h-screen relative z-0">{children}</div>
+              <div className="min-h-[calc(100vh-5rem)] relative z-0">
+                {children}
+              </div>
             </SignedIn>
           </div>
         </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <div className="w-full grid place-items-center min-h-screen">Home</div>
-  );
+  return <div></div>;
 }
